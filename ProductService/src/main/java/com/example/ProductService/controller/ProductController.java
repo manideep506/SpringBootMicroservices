b216ecfefb9bced1f6d629afead8b4b78ac42daa
manid <manideep0411@gmail.com>
2024-04-29 16:18:18 +0530

    new api's added

--- ProductService/src/main/java/com/example/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/example/ProductService/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.ProductService.controller;
 
+import com.example.ProductService.entity.Product;
 import com.example.ProductService.exception.ProductServiceException;
 import com.example.ProductService.model.ProductRequest;
 import com.example.ProductService.model.ProductResponse;
@@ -9,11 +10,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
     @Autowired
     private ProductService productService;
+
+    @GetMapping("/allProducts")
+    public ResponseEntity<List<Product>> getAllQuestions(){
+        return productService.getProducts();
+    }
+
     @PostMapping
     public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest){
         Long productId = productService.addProduct(productRequest);
@@ -23,6 +32,11 @@ public class ProductController {
     public ResponseEntity<ProductResponse> getProduct(@PathVariable("id") Long productId) throws ProductServiceException {
         ProductResponse response = productService.getProductById(productId);
         return new ResponseEntity<ProductResponse>(response,HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long productId) throws ProductServiceException {
+        productService.deleteProductById(productId);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
     @PutMapping("/reduceQuantity/{id}")
--- ProductService/src/main/java/com/example/ProductService/service/ProductService.java
+++ ProductService/src/main/java/com/example/ProductService/service/ProductService.java
@@ -1,8 +1,12 @@
 package com.example.ProductService.service;
 
+import com.example.ProductService.entity.Product;
 import com.example.ProductService.exception.ProductServiceException;
 import com.example.ProductService.model.ProductRequest;
 import com.example.ProductService.model.ProductResponse;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface ProductService {
     Long addProduct(ProductRequest productRequest);
@@ -10,4 +14,8 @@ public interface ProductService {
     ProductResponse getProductById(Long productId) throws ProductServiceException;
 
     void reduceQuantity(Long productId, int quantity) throws ProductServiceException;
+
+    ResponseEntity<List<Product>> getProducts();
+
+    void deleteProductById(Long productId);
 }
