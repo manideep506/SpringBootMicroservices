f03ac391dd6d9cee50965e0c20600f7715c4f4d0
manid <manideep0411@gmail.com>
2024-04-30 15:55:48 +0530

    new service and some existing changes

---
+++ OrderService/src/main/java/com/example/OrderService/model/PaymentRequest.java
@@ -0,0 +1,16 @@
+package com.example.OrderService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentRequest {
+    private long orderId;
+    private PaymentMode paymentMode;
+    private double amount;
+}
--- OrderService/src/main/java/com/example/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/example/OrderService/service/OrderServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.OrderService.service;
 
+import com.ctc.wstx.util.StringUtil;
+import com.example.OrderService.client.PaymentService;
 import com.example.OrderService.client.ProductService;
 import com.example.OrderService.entity.Order;
 import com.example.OrderService.model.OrderRequest;
+import com.example.OrderService.model.PaymentRequest;
 import com.example.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +19,8 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private PaymentService paymentService;
     @Autowired
     private ProductService productService;
 
@@ -33,6 +39,24 @@ public class OrderServiceImpl implements OrderService {
         log.info("order placed");
         order =orderRepository.save(order);
         log.info("product saved successfully after placing order");
+
+        PaymentRequest paymentRequest = PaymentRequest.builder()
+                .amount (orderRequest.getAmount())
+                .paymentMode (orderRequest.getPaymentMode())
+                .orderId (order.getOrderId())
+                .build();
+        log.info("paymentRequest 55 "+paymentRequest);
+        String status = StringUtils.EMPTY;
+        try{
+            paymentService.doPayment(paymentRequest);
+            status = "Success";
+        }catch (Exception e){
+            log.info("payment failed" +paymentRequest);
+            status = "Failed";
+        }
+        order.setOrderStatus(status);
+        order = orderRepository.save(order);
+        log.info("order was suceesfull ");
         return order.getOrderId();
     }
 }
