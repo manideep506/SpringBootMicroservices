5a0e31c6af2353a23c8b00face852e98ad39a6ff
manid <manideep0411@gmail.com>
2024-04-30 16:32:10 +0530

    proper alignment

--- CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CloudGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-
 }
